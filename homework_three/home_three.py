3d1cac7c2329388ad29731b5e47fee9d55274439
BilimFrontend <erkinbekov298@gmail.com>
2024-10-09 20:56:47 +0600

    homework_three

---
+++ homework_three/home_three.py
@@ -0,0 +1,19 @@
+word = input("Введите любое слово: ")
+vowels = "aeiouyаиоуыэ"
+consonants = "bcdfghjklmnpqrstvwxyzбвгджзйклмнпрстфхцчшщ"
+letters = 0
+vowels_count = 0
+consonants_count = 0
+
+for letter in word.lower():
+    letters += 1
+    for consonant in consonants:
+        if consonant in letter:
+            consonants_count += 1
+    for vowel in vowels:
+        if vowel in letter:
+            vowels_count += 1
+
+percent_vowels = (vowels_count / letters) * 100
+percent_consonants = (consonants_count / letters) * 100
+print(f"Слово: {word}\nКоличество букв: {letters}\nСогласных букв: {consonants_count}\nГласных букв: {vowels_count}\nГласные/Согласные: {percent_vowels}%/{percent_consonants}%")
